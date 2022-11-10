ecbd403177d880093552e5eb34fbb5c3679c7002
Anand S <anandzzz360@gmail.com>
2022-11-10 21:06:39 +0530

    NOT Gate

---
+++ not_gate/not_gate.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:55:42 03/07/2022 
+// Design Name: 
+// Module Name:    not_gate 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module not_gate(
+    input a,
+    output b
+    );
+not(b,a);
+endmodule
